aa92ea1e28e856aaefadf2c5b853da30a02ab856
ignas rocas <ignasandholly@gmail.com>
2021-04-20 12:57:25 +0100

    added drawer layout

--- app/src/main/java/com/ignas/android/groceryshoppingapp/MainActivity.java
+++ app/src/main/java/com/ignas/android/groceryshoppingapp/MainActivity.java
@@ -3,11 +3,21 @@ package com.ignas.android.groceryshoppingapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.BaseAdapter;
+import android.widget.EditText;
+import android.widget.HeaderViewListAdapter;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
 import androidx.viewpager.widget.ViewPager;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.tabs.TabItem;
 import com.google.android.material.tabs.TabLayout;
 import com.ignas.android.groceryshoppingapp.Logic.dbHelper;
@@ -24,6 +34,10 @@ public class MainActivity extends AppCompatActivity {
         TabItem manageItems;
         Toolbar toolbar;
 
+        NavigationView mNavigationView;
+        DrawerLayout drawerLayout;
+        final MenuItem[] previousMenuItem = {null};
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -31,14 +45,30 @@ public class MainActivity extends AppCompatActivity {
         tabLayout = findViewById(R.id.tabs);
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        mNavigationView = findViewById(R.id.navigation_view);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        mNavigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        if(previousMenuItem[0] !=null){
+                            previousMenuItem[0].setChecked(false);
+                        }
+                        previousMenuItem[0] = menuItem;
+                        menuItem.setChecked(true);
+                        drawerLayout.closeDrawers();
+                        return true;
+                    }
+                });
+
 
         manageItems = findViewById(R.id.ItemFragment);
         viewPager = findViewById(R.id.pageView);
-        tabAdapter = new TabAdapter(getSupportFragmentManager(),tabLayout.getTabCount());
+        tabAdapter = new TabAdapter(fragmentManager,tabLayout.getTabCount());
         viewPager.setAdapter(tabAdapter);
         viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
-
-
 
         tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener(){
 
@@ -59,7 +89,27 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
+
+    }
+    public void refreshDrawer(){
+        for (int i = 0, count = mNavigationView.getChildCount(); i < count; i++) {
+            final View child = mNavigationView.getChildAt(i);
+            if (child != null && child instanceof ListView) {
+                final ListView menuView = (ListView) child;
+                final HeaderViewListAdapter adapter = (HeaderViewListAdapter) menuView.getAdapter();
+                final BaseAdapter wrapped = (BaseAdapter) adapter.getWrappedAdapter();
+                wrapped.notifyDataSetChanged();
+            }
         }
+    }
+
 
     @Override
     protected void onStop() {
@@ -72,9 +122,7 @@ public class MainActivity extends AppCompatActivity {
             intent.putExtra("time",itemToBeScheduled.getRunOutDate().getTime());
             this.startService(intent);
             Log.i("log", "re_scheduleAlarm: "+itemToBeScheduled.getItemName());
-
         }
-            Log.d(TAG, "onStop: come back");
     }
 
     @Override
